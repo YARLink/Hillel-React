20ec2433b8545d28fc2d8aa501e140f1151e1eb3
Yaroslav Atamanets <yarik.atamanets@gmail.com>
2025-02-01 02:08:38 +0200

    render animal list as a table with sequential row activation

---
+++ render-list/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import List from "./components/List/List";
+
+export default function App() {
+  return (
+    <>
+      <h1>Animals Table</h1>
+      <List />
+    </>
+  );
+}
