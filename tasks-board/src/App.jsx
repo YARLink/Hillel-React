70580ad980feaa71dc88ff7a7fb6b19c4e9559ea
Yaroslav Atamanets <yarik.atamanets@gmail.com>
2025-02-17 00:27:47 +0200

    implemented task status change using constants and updated rendering logic

---
+++ tasks-board/src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import TaskBoard from "./components/TaskBoard";
+
+export default function App() {
+  return (
+    <div className="App">
+      <TaskBoard />
+    </div>
+  );
+}
